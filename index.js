db3681a15c866fbbba32e6ac4c1a9811c3558033
Scott McKenzie <slammayjammay@gmail.com>
2017-01-19 20:18:11 -0800

    Check if input is correct length

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const RubiksCube = require('./rubiks-cube')
 
 class Solver {
   constructor(cubeState) {
+    if (cubeState.length !== 9 * 6) {
+      throw new Error('Wrong number of colors provided');
+    }
     this.cube = new RubiksCube(cubeState)
   }
 }
