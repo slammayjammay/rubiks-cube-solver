47de93f8a481c80d943ecbe22da6b7ce805d7125
Scott McKenzie <slammayjammay@gmail.com>
2017-01-29 15:41:13 -0800

    Refactor

--- models/Cubie.js
+++ models/Cubie.js
@@ -71,6 +71,13 @@ class Cubie {
   }
 
   /**
+   * @return {array}
+   */
+  colors() {
+    return Object.keys(this._normalToColor).map(normal => this._normalToColor[normal])
+  }
+
+  /**
    * @param {string} color - Check if the cubie has this color.
    * @return {boolean}
    */
@@ -88,12 +95,12 @@ class Cubie {
    * Sets a color on a given face or normal of a cubie.
    * @param {string} face - The face of the cubie we want to set the color on.
    * @param {string} color - The color we want to set.
-   * @return {null}
+   * @return {Cubie}
    */
-  setColor(face, color) {
-
+  colorFace(face, color) {
     let normal = Face.getNormal(face).join(' ')
     this._normalToColor[normal] = color
+    return this
   }
 
   /**
@@ -114,7 +121,7 @@ class Cubie {
       return this._normalToColor[cubieColor] === color
     })
 
-    return Face.findFace(normal)
+    return Face.getFace(normal)
   }
 
   /**
@@ -156,114 +163,6 @@ class Cubie {
 
     this._normalToColor = newMap
   }
-
-  // /**
-  //  * Returns a normal-to-color map based on the rotation axis and the new
-  //  * position after the rotation. This will later be assigned to `this.pos`.
-  //  * NOTE: After every rotation, the normal-color-map will need to be updated,
-  //  * but the normal that equals the axis rotation will not have its color
-  //  * changed.
-  //  *
-  //  * @param {string} axis - The axis of rotation.
-  //  * @param {array} newPos - The new position vector.
-  //  * @return {object} - The new normal-color map.
-  //  */
-  // _getNewColors(axis, newPos) {
-  //   let ret = {}
-  //
-  //   // get the current and updated array of normals, sorted as 'x', 'y', 'z'
-  //   let normals = this._sortNormals(Object.keys(this._normalToColor))
-  //   let newNormals = this._createNormals(newPos)
-  //
-  //   // find the normal that "matches" the rotation axis (sort of hack-y)
-  //   let rotationNormal
-  //   for (let normal of normals) {
-  //     if (this._getAxisOfNormal(normal) === axis) {
-  //       rotationNormal = normal
-  //       break
-  //     }
-  //   }
-  //
-  //   // splice out the rotation normal from the normal arrays, as this is the
-  //   // one normal-color pair that will not change.
-  //   normals.splice(normals.indexOf(rotationNormal), 1)
-  //   newNormals.splice(newNormals.indexOf(rotationNormal), 1)
-  //
-  //   ret[rotationNormal] = this._normalToColor[rotationNormal]
-  //
-  //   // Different for corners, edges, and middles......
-  //   if (normals.length === 0) {
-  //     return ret
-  //   } else if (normals.length === 1) {
-  //     ret[newNormals[0]] = this._normalToColor[normals[0]]
-  //   } else if (normals.length === 2) {
-  //     ret[newNormals[0]] = this._normalToColor[normals[1]]
-  //     ret[newNormals[1]] = this._normalToColor[normals[0]]
-  //   }
-  //
-  //   return ret
-  // }
-
-  // /**
-  //  * Finds the index of the first non-zero value. Returns 'x', 'y', or 'z' if
-  //  * the index is 0, 1, or 2 respectively.
-  //  * @param {array} normal - The vector representing a normal.
-  //  */
-  // _getAxisOfNormal(normal) {
-  //   let axes = normal.split(' ')
-  //   let index = axes.findIndex(value => parseInt(value) !== 0)
-  //
-  //   if (index === 0) return 'x'
-  //   if (index === 1) return 'y'
-  //   if (index === 2) return 'z'
-  // }
-
-  // /**
-  //  * @param {array} normals - The array of strings representing normals.
-  //  */
-  // _sortNormals(normals) {
-  //   return normals.sort((first, second) => {
-  //     let firstIdx = first.split(' ').findIndex(value => parseInt(value) !== 0)
-  //     let secondIdx = second.split(' ').findIndex(value => parseInt(value) !== 0)
-  //     return firstIdx - secondIdx
-  //   })
-  // }
-
-  // /**
-  //  * @param {array} pos - The position vector.
-  //  * @return {array} - An array of strings representing normals.
-  //  */
-  // _createNormals(pos) {
-  //   let normals = []
-  //
-  //   for (let i = 0; i < pos.length; i++) {
-  //     if (pos[i] === 0) {
-  //       continue
-  //     }
-  //     let newNormal = [0, 0, 0]
-  //     newNormal[i] = pos[i]
-  //     normals.push(newNormal.join(' '))
-  //   }
-  //
-  //   return normals
-  // }
-
-  // /**
-  //  * Creates and returns a vector setting the given axis with the given mag,
-  //  * and all other axes with a value of 0.
-  //  * @return {array}
-  //  */
-  // _newVector(axis, mag) {
-  //   axis = axis.toLowerCase()
-  //
-  //   if (axis === 'x') {
-  //     return [mag, 0, 0]
-  //   } else if (axis === 'y') {
-  //     return [0, mag, 0]
-  //   } else if (axis === 'z') {
-  //     return [0, 0, mag]
-  //   }
-  // }
 }
 
 module.exports = Cubie
