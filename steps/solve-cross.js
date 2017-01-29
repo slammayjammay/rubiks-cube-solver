93114f113f8b1391668e511d3926a83ed11d5074
Scott McKenzie <slammayjammay@gmail.com>
2017-01-29 15:41:48 -0800

    Test cases

--- steps/solve-cross.js
+++ steps/solve-cross.js
@@ -1,3 +1,6 @@
+const Cubie = require('../models/Cubie')
+const getAdjacentFaceDirection = require('../utils/relative-faces').getAdjacentFaceDirection
+
 const CROSS_COLOR = 'U'
 
 class CrossSolver {
@@ -5,9 +8,9 @@ class CrossSolver {
    * Solves the first step following the Fridich Method: the cross. Solves the
    * cross on the UP face by default.
    *
-   * @param {string|RubiksCube} rubiksCube - This can either be a 54-character long
-   * string representing the cube state (in this case it will have to "build"
-   * another rubik's Cube), or an already built RubiksCube object.
+   * @param {string|RubiksCube} rubiksCube - This can either be a 54-character
+   * long string representing the cube state (in this case it will have to
+   * "build" another rubik's Cube), or an already built RubiksCube object.
    */
   constructor(rubiksCube) {
     this.cube = typeof rubiksCube === 'string' ? new RubiksCube(rubiksCube) : rubiksCube
@@ -30,6 +33,15 @@ class CrossSolver {
   }
 
   /**
+   * @param {cubie} edge - The edge that will be solved.
+   */
+  _solveEdge(edge) {
+    let caseNumber = this._getCaseNumber(edge)
+    console.log(caseNumber)
+    console.log(edge.colors())
+  }
+
+  /**
    * 6 Cases!
    * 1) The edge's UP color is on the UP face.
    * 2) the edge's UP color is on the DOWN face.
@@ -38,14 +50,6 @@ class CrossSolver {
    * 5) The edge's UP color is not on the UP or DOWN face and the other color is on the RELATIVE RIGHT face.
    * 6) The edge's UP color is not on the UP or DOWN face and the other color is on the RELATIVE LEFT face.
    *
-   * @param {cubie} edge - The edge that will be solved.
-   */
-  _solveEdge(edge) {
-    let caseNumber = this._getCaseNumber(edge)
-    // console.log(caseNumber)
-  }
-
-  /**
    * @param {cubie} edge
    */
   _getCaseNumber(edge) {
@@ -61,22 +65,45 @@ class CrossSolver {
       return 4
     }
 
-    // check which side the "other color's face" is, relative to the cross color's face.
-    let [crossFace, otherFace] = edge.faces().sort((face1, face2) => {
-      return face1 === edge.getFaceOfColor(CROSS_COLOR) ? -1 : 1
-    })
+    let crossFace = edge.getFaceOfColor(CROSS_COLOR)
+    let otherFace = edge.getFaceOfColor(edge.colors().find(color => color !== CROSS_COLOR))
+    let direction = getAdjacentFaceDirection(crossFace, otherFace, { UP: 'UP' })
 
-    let direction = rubiksCube._getAdjacentFaceDirection(crossFace, otherFace, { UP: 'UP' })
     if (direction === 'RIGHT') {
       return 5
     } else if (direction === 'LEFT') {
       return 6
     }
   }
+
+  testCases() {
+    let caseNumber, message
+    let edge
+    let [crossColor, otherColor] = ['U', 'R']
+
+    // case 1
+    edge = new Cubie([0, 1, 1]).colorFace('UP', crossColor).colorFace('FRONT', otherColor)
+    console.log(this._getCaseNumber(edge) === 1 ? 'SUCCESS' : 'FAIL')
+    // case 2
+    edge = new Cubie([0, -1, 1]).colorFace('DOWN', crossColor).colorFace('FRONT', otherColor)
+    console.log(this._getCaseNumber(edge) === 2 ? 'SUCCESS' : 'FAIL')
+    // case 3
+    edge = new Cubie([1, 1, 0]).colorFace('RIGHT', crossColor).colorFace('UP', otherColor)
+    console.log(this._getCaseNumber(edge) === 3 ? 'SUCCESS' : 'FAIL')
+    // case 4
+    edge = new Cubie([1, -1, 0]).colorFace('RIGHT', crossColor).colorFace('DOWN', otherColor)
+    console.log(this._getCaseNumber(edge) === 4 ? 'SUCCESS' : 'FAIL')
+    // case 5
+    edge = new Cubie([1, 0, -1]).colorFace('RIGHT', crossColor).colorFace('BACK', otherColor)
+    console.log(this._getCaseNumber(edge) === 5 ? 'SUCCESS' : 'FAIL')
+    // case 6
+    edge = new Cubie([1, 0, 1]).colorFace('RIGHT', crossColor).colorFace('FRONT', otherColor)
+    console.log(this._getCaseNumber(edge) === 6 ? 'SUCCESS' : 'FAIL')
+  }
 }
 
 module.exports = (rubiksCube) => {
   let crossSolver = new CrossSolver(rubiksCube)
-  crossSolver.solve(rubiksCube)
+  // crossSolver.solve(rubiksCube)
   // return crossSolver.getMoves()
 }
