afef458b2977d05a6ca5d4bbfe8b64696fe6db5d
Scott McKenzie <slammayjammay@gmail.com>
2017-03-04 22:50:36 -0800

    Detect and solve case 1

--- solvers/f2l/index.js
+++ solvers/f2l/index.js
@@ -1,4 +1,5 @@
 const BaseSolver = require('./BaseSolver')
+const case1Solver = require('./case-1')
 const RubiksCube = require('../../models/RubiksCube')
 const Cubie = require('../../models/Cubie')
 
@@ -6,6 +7,17 @@ class F2LSolver extends BaseSolver {
   solve() {
     let pairs = this.getAllPairs()
     pairs.forEach(pair => this.solvePair(pair))
+  }
+
+  isSolved() {
+    let pairs = this.getAllPairs()
+    for (let pair of pairs) {
+      if (!this.isPairSolved(pair)) {
+        return false
+      }
+    }
+
+    return true
   }
 
   getAllPairs() {
@@ -28,18 +40,29 @@ class F2LSolver extends BaseSolver {
     return pairs
   }
 
-   /**
-    * 5 top level cases:
-    * 1) corner and edge are both on the DOWN face
-    * 2)
-    * 3)
-    * 4)
-    * 5)
-    */
   solvePair({ corner, edge }) {
-    if (corner.faces().includes('DOWN') && edge.faces().includes('DOWN')) {
+    let caseNum = this._getCaseNumber({ corner, edge })
+    if (!caseNum) return
+    this[`_solveCase${caseNum}`]({ corner, edge })
+  }
 
+  /**
+   * 5 top level cases:
+   * 1) corner and edge are both on the DOWN face
+   * 2)
+   * 3)
+   * 4)
+   * 5)
+   */
+  _getCaseNumber({ corner, edge }) {
+    if (corner.faces().includes('DOWN') && edge.faces().includes('DOWN')) {
+      return 1
     }
+  }
+
+  _solveCase1({ corner, edge }) {
+    let solveMoves = new case1Solver({ corner, edge })
+    this.move(solveMoves)
   }
 }
 
